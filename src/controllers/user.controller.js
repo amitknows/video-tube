ba51dab902aa41daa0d864d891ae30c1345a9f11
amitknows <amitknowsyou@gmail.com>
2024-06-14 15:16:17 +0530

    login, logout, auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating refresh and access token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -33,63 +50,140 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
+  //Future assignment check validation on valid email, username, password
 
-//Future assignment check validation on valid email, username, password
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
+  if (existedUser) {
+    throw new ApiError(409, "User already exist");
+  }
 
+  const avatarLocalPath = req.files?.avatar[0]?.path; //we are chaining here ptani mile ya na mile. what is chaining?            //files is coming from multer
+  //const coverImageLocalPath = req.files?.coverImage[0].path;
+  //console.log("avatar local path: ", avatarLocalPath)
+  //avatar is required so we will check here
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required in localpath");
+  }
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+  //so now we have local path of avatar we will upload it on cloudinary using the uploadOnCloudinary method imported from utils folder
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  console.log("avatar cloudinary : ", avatar);
+  //again we will check for avatar
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
 
-const existedUser =await User.findOne({
-    $or: [{ username }, { email }]
-})
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
 
-if(existedUser){
-  throw new ApiError(409, "User already exist")
-}
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-const avatarLocalPath = req.files?.avatar[0]?.path;   //we are chaining here ptani mile ya na mile. what is chaining?            //files is coming from multer
-//const coverImageLocalPath = req.files?.coverImage[0].path;
-//console.log("avatar local path: ", avatarLocalPath)
-//avatar is required so we will check here 
-if(!avatarLocalPath){
-  throw new ApiError(400, "Avatar file is required in localpath")
-}
-let coverImageLocalPath
-if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0 ){
-   coverImageLocalPath = req.files.coverImage[0].path
-}
-//so now we have local path of avatar we will upload it on cloudinary using the uploadOnCloudinary method imported from utils folder
-const avatar = await uploadOnCloudinary(avatarLocalPath)
-const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-console.log("avatar cloudinary : ", avatar)
-//again we will check for avatar
-if(!avatar){
-  throw new ApiError(400, "Avatar file is required")
-}
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wronh while registering the user");
+  }
 
-
-
-const user = await User.create({
-  fullName,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username:username.toLowerCase(),
-})
-
-const createdUser = await User.findById(user._id).select(
-  '-password -refreshToken'
-)
-
-if(!createdUser){
-  throw new ApiError(500, "Something went wronh while registering the user")
-}
-
-
-return res.status(201).json(
-  new ApiResponse(200, createdUser, "User registered successfully")
-)
-
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
-export { registerUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  // get login details from req.body
+  // username or email check
+  // search username or email in database
+  // validate password
+  // generate access and refresh token
+  // save refresh in database
+  // send tokens in cookies
+  // send user data
+
+  const { username, email, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }], // or is a mongodb method
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(404, "Invalid User Credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+
+const logoutUser = asyncHandler(async (req, res)=>{
+ await User.findByIdAndUpdate(
+   req.user._id,
+   {
+    $set:{
+      refreshToken: undefined
+    }
+   },
+   {
+    new:true
+   }
+  )  
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("accessToken", options)
+})
+
+export { registerUser, loginUser, logoutUser };
